175a74e3de42e8fe367a35a33593b27938085c42
franbsza <franciele.bsza@gmail.com>
2023-04-03 19:41:57 -0300

    controllers

---
+++ src/main/java/com/digital/helpdesk/controllers/RoleController.java
@@ -0,0 +1,39 @@
+package com.digital.helpdesk.controllers;
+
+import com.digital.helpdesk.models.Role;
+import com.digital.helpdesk.service.RoleService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@AllArgsConstructor
+@Controller
+@RequestMapping("/roles")
+public class RoleController {
+
+    private final RoleService service;
+
+    @GetMapping
+    public String index(Model model){
+        return "roles/index";
+    }
+    @GetMapping("/new")
+    public String create(Model model){
+        model.addAttribute("role", new Role());
+        return "roles/create";
+    }
+    @PostMapping
+    public String save(@Valid @ModelAttribute("role") Role role, BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors()){
+            return "redirect:/roles/new";
+        }
+        Role roleCreated = service.create(role);
+        return "redirect:/roles";
+    }
+
+}
